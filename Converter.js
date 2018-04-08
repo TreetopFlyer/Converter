dc3a9ab706e254d028e1b702f8758007e0e52982
Seth Trowbridge <seth111@gmail.com>
2018-04-08 08:26:04 -0400

    Update Converter.js

--- Converter.js
+++ Converter.js
@@ -1,40 +1,8 @@
-/*
-	methods:
-
-	QueryStringAsObject(); // return an object based on the Query String
-	ObjectAsQueryString(inObject); // set the Query String based on "inObject" (will case a reload, and will remove pruned values)
-	
-	CookieAsObject(); // return an object based on the Cookies
-	ObjectAsCookie(inObject, inHours=1); // set the Cookies based on "inObject" (will remove pruned cookies. all remaining cookies will be reset with a duration of "inHours" [inHours defaults to 1 of not provided])
-
-	examples:
-	
-	// Add/change Query String value:
-	var object = Converter.QueryStringAsObject();
-	object.NewProperty = "New Value";
-	Converter.ObjectAsQueryString(object);
-	
-	// Remove QueryString value:
-	var object = Converter.QueryStringAsObject();
-	object.OldProperty = null;
-	Converter.ObjectAsQueryString(object);	
-	
-	// Add/change Cookie:
-	var object = Converter.CookiesAsObject();
-	object.NewCookieName = "New Cookie Value";
-	Converter.ObjectAsCookies(object);
-	
-	// Remove Cookie(s):
-	var object = Converter.CookiesAsObject();
-	object.someoldcookie = null;
-	Converter.ObjectAsCookies(object);	
-	
-*/
-
 var Converter = {};
 Converter.Utility = {};
 Converter.Utility.Splitter = function(inString, inSplitter, inSesitivityRoutine)
 {
+    var i;
     var output = {};
     var segments = inString.split(inSplitter);
     for (i = 0; i < segments.length; i++) {
@@ -67,12 +35,17 @@ Converter.Config.Case = Converter.Utility.CaseInsesitive;
 
 Converter.QueryToObject = function (inString)
 {
+    if(arguments.length == 0)
+    {
+        inString = window.location.search;
+    }
+	
 	if(inString[0] == "?")
 		inString = inString.substring(1);
 
 	return Converter.Utility.Splitter(inString, "&", Converter.Config.Case);
 };
-Converter.ObjectToQuery = function (inObj)
+Converter.ObjectToQuery = function (inObj, inSet)
 {
     var output = "";
     var property;
@@ -85,39 +58,53 @@ Converter.ObjectToQuery = function (inObj)
 	output = "?" + output.substring(1);
 	
 	if(output.length == 1)
-		return "";
-	else
-		return output;
+		output = "";
+	
+	if(inSet)
+	{
+	    window.location.search = output;
+	}
+	return output;
 };
 Converter.CookieToObject = function (inString)
 {
+    if(arguments.length == 0)
+    {
+        inString = document.cookie;
+    }
 	return Converter.Utility.Splitter(inString, "; ", Converter.Config.Case);
 };
-Converter.ObjectToCookie = function (inObj)
+Converter.ObjectToCookie = function (inObj, inSet)
 {
     var property;
     var value;
     var hours;
 	var output = "";
-    if (arguments.length == 2)
-        hours = arguments[1];
-    else
-        hours = Converter.Config.Hours;
 
     var d = new Date();
     d.setTime(d.getTime() + (hours * 60 * 60 * 1000));
     for (property in inObj)
 	{
         value = inObj[property];
-        if (value !== null)
-            document.cookie = (property + "=" + value + "; expires=" + d.toGMTString());
+        if (value != null)
+            output += (property + "=" + value + ";path=/;expires=" + d.toGMTString());
         else
-            document.cookie = (property + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+            output += (property + "=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT");
+    }
+    
+    if(inSet)
+    {
+        document.cookie = output;
     }
 	return output;
 };
-Converter.SlashPathToObject = function(inPath)
+Converter.PathToObject = function(inPath)
 {
+    if(arguments.length == 0)
+    {
+        inPath = window.location.pathname;
+    }
+    
 	var i;
 	var count = 0;
 	var current = false;
@@ -135,24 +122,18 @@ Converter.SlashPathToObject = function(inPath)
 	}
 	return obj;
 };
-Converter.ObjectToSlashPath = function(inObj)
+Converter.ObjectToPath = function(inObj, inSet)
 {
-	var i;
+	var prop;
 	var output = "";
 	for(prop in inObj)
 	{
 		output += "/"+inObj[prop];
 	}
+	
+	if(inSet)
+	{
+	    window.location.pathname = output;
+	}
 	return output;
 };
-
-Converter.State = {};
-Converter.State.Cookie = Converter.CookieToObject(document.cookie);
-Converter.State.Query = Converter.QueryToObject(window.location.search);
-Converter.State.SlashPath = Converter.SlashPathToObject(window.location.pathname);
-Converter.State.Commit = function()
-{
-	Converter.ObjectToCookie(Converter.State.Cookie);
-	window.location = window.location.origin + Converter.ObjectToSlashPath(Converter.State.SlashPath) + Converter.ObjectToQuery(Converter.State.Query);
-};
-
